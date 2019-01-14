d966700db65aa34f7ed757f14c22166610bb4339
Chris Pak <chris.pak125@gmail.com>
2019-01-14 17:03:02 -0500

    Set up recipes controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,29 @@
+class RecipesController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        @recipes = current_user.recipes
+        render json: @recipes
+    end
+
+    def show
+        @recipe = Recipe.find(params[:id])
+        render json: @recipe
+    end
+
+    def create
+        @user = current_user
+        @recipe = @user.recipes.create!(recipe_params)
+    end
+
+    def destroy
+        @recipe = Recipe.find(params[:id]).destroy
+        render json: 200
+    end
+
+private
+def recipe_params
+    params.require(:recipe).permit(:name, :ingredients, :cal_per_serving, :servings, :instructions, :img)
+end
+
+end
