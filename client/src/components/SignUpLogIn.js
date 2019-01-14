9416b1870e320ecddae4861581c52524aff5b470
Chris Pak <chris.pak125@gmail.com>
2019-01-14 15:31:20 -0500

    Created SignUpLogIn component

---
+++ client/src/components/SignUpLogIn.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class SignUpLogIn extends Component {
+    state={
+        email:'',
+        password:'',
+        password_confirmation:''
+    }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
