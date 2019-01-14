5da0c5daf6a9af8f0cee658f8a911ff2dad3fd60
Chris Pak <chris.pak125@gmail.com>
2019-01-14 15:56:06 -0500

    Created signUp and signIn functions and signUp route

--- client/src/components/SignUpLogIn.js
+++ client/src/components/SignUpLogIn.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react'
 
 export default class SignUpLogIn extends Component {
-    state={
-        email:'',
-        password:'',
-        password_confirmation:''
+    state = {
+        email: '',
+        password: '',
+        password_confirmation: ''
     }
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+
+    signUp = (event) => {
+        event.preventDefault()
+        this.props.signUp(
+            this.state.email,
+            this.state.password,
+            this.state.password_confirmation
+        )
+    }
+
+    signIn = (event) => {
+        event.preventDefault()
+        this.props.signIn(
+            this.state.email,
+            this.state.password
+        )
+    }
+
+    handleChange = (event) => {
+        const newState = { ...this.state }
+        newState[event.target.name] = event.target.value
+        this.setState(newState)
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <div>
+                        <label htmlFor="email">E-mail: </label>
+                        <input onChange={this.handleChange} type="text" name="email" value={this.state.email} />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password: </label>
+                        <input onChange={this.handleChange} type="password" name="password" value={this.state.password} />
+                    </div>
+                    <div>
+                        <label htmlFor="password_confirmation">Confirm Password: </label>
+                        <input onChange={this.handleChange} type="password" name="password_confirmation"
+                            value={this.state.password_confirmation} />
+                    </div>
+
+                    <button onClick={this.signUp}>Sign Up</button>
+                    <button onClick={this.signIn}>Log In</button>
+                </form>
+            </div>
+        )
+    }
 }
