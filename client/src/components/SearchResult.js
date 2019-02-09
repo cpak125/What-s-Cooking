394ce66dc4a3e3799121504ef4d60eb0265e525a
Chris Pak <chris.pak125@gmail.com>
2019-02-09 14:01:38 -0500

    Created result preview component

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react'
+import Preview from './Preview';
 
 export default class SearchResult extends Component {
+    state = {
+        openPreview: false
+    }
 
     addNewRecipe = (name, ingredients, servings, calories, instructions, img) => {
         this.props.addNewRecipe(name, ingredients, servings, calories, instructions, img)
         this.props.handleClose()
         this.props.toggleAddRecipe()
     }
+
+    togglePreview = () => {
+        this.setState({ openPreview: !this.state.openPreview })
+    }
+
     render() {
         return (
             <div>
-                <img src={this.props.img} alt='recipe img' />
-                <div>{this.props.name}</div>
-                <button onClick={()=> this.addNewRecipe(
-                    this.props.name, this.props.ingredients, this.props.servings, 
-                    this.props.calories, this.props.instructions, this.props.img)}
-                    > Add to Recipes
-                    </button>
-
+            {this.state.openPreview ? 
+                <Preview /> :
+                    <div onClick={this.togglePreview}>
+                        <h2>{this.props.name}</h2>
+                        <img src={this.props.img} alt='recipe img' />
+                        {/* <button onClick={() => this.addNewRecipe(
+                            this.props.name, this.props.ingredients, this.props.servings,
+                            this.props.calories, this.props.instructions, this.props.img)}
+                        > Add to Recipes
+                </button> */}
+                    </div>}
             </div>
+
         )
     }
 }
